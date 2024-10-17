ad9a5b76afa4438c3a719d498778701f31a0b862
Patrick Valsecchi <patrick@thus.ch>
2024-10-17 20:22:38 +0200

    Use a lib for the button

--- main/main.c
+++ main/main.c
@@ -1,13 +1,31 @@
-#include "button.h"
+#include "iot_button.h"
 #include "led.h"
 #include "zigbee.h"
+#include <esp_log.h>
 #include <nvs_flash.h>
 #include <soc/gpio_num.h>
 #include <stdio.h>
 
-static ButtonState buttonState;
+static const char *TAG = "main";
+static void long_press(void *arg, void *usr_data) { zigbee_reset_pairing(); }
 
-static void app_long_press(void *arg) { zigbee_reset_pairing(); }
+static void setup_button() {
+    button_config_t gpio_btn_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config =
+            {
+                .gpio_num = GPIO_NUM_9,
+                .active_level = 0,
+            },
+    };
+    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
+    if (NULL == gpio_btn) {
+        ESP_LOGE(TAG, "Button create failed");
+    }
+    iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_START, long_press, NULL);
+}
 
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -16,6 +34,5 @@ void app_main(void) {
 
     zigbee_init();
 
-    button_init();
-    button_add(&buttonState, GPIO_NUM_9, NULL, app_long_press, NULL);
+    setup_button();
 }
