450eec496ce2b37f6521df0822a16590d43ab574
Patrick Valsecchi <patrick@thus.ch>
2024-10-17 19:17:47 +0200

    Refactor the button part to be re-usable

--- main/button.h
+++ main/button.h
@@ -1,3 +1,17 @@
 #pragma once
 
-void button_init(void);
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <inttypes.h>
+
+typedef void (*ButtonCb)(void *);
+
+typedef struct {
+  int64_t pressedTimeUs;
+  ButtonCb shortCb;
+  ButtonCb longCb;
+  void *arg;
+} ButtonState;
+
+void button_init();
+void button_add(ButtonState *buttonState, uint32_t gpio, ButtonCb shortCb, ButtonCb longCb, void *arg);
--- main/main.c
+++ main/main.c
@@ -2,11 +2,20 @@
 #include "led.h"
 #include "zigbee.h"
 #include <nvs_flash.h>
+#include <soc/gpio_num.h>
 #include <stdio.h>
+
+static ButtonState buttonState;
+
+static void app_long_press(void *arg) { zigbee_reset_pairing(); }
 
 void app_main(void) {
   ESP_ERROR_CHECK(nvs_flash_init());
+
   led_init();
+
   zigbee_init();
+
   button_init();
+  button_add(&buttonState, GPIO_NUM_9, NULL, app_long_press, NULL);
 }
