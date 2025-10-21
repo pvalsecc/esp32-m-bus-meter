66a871a8000041aaf664fa392fa9058d887d0d08
Patrick Valsecchi <patrick@thus.ch>
2025-10-21 16:42:23 +0200

    Upgrade dependencies

--- main/led.c
+++ main/led.c
@@ -1,6 +1,8 @@
 #include "led.h"
 #include <esp_log.h>
 #include <led_indicator.h>
+#include <led_indicator_gpio.h>
+#include <led_indicator_strips.h>
 
 static const char *TAG = "led";
 
@@ -30,34 +32,29 @@ void led_set(RgbColor color, uint16_t intensity) {
 void led_blink(BlinkType type) { ESP_ERROR_CHECK(led_indicator_start(led_handle, type)); }
 
 void led_init(void) {
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = 8,                      // The GPIO that connected to the LED strip's data line
-        .max_leds = 1,                            // The number of LEDs in the strip,
-        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
-        .led_model = LED_MODEL_WS2812,            // LED strip model
-        .flags.invert_out = false,                // whether to invert the output signal
-    };
-
-    // LED strip backend configuration: RMT
-    led_strip_rmt_config_t rmt_config = {
-        .clk_src = RMT_CLK_SRC_DEFAULT, // different clock source can lead to different power consumption
-        .resolution_hz = 0,             // RMT counter clock frequency
-        .flags.with_dma = false,        // DMA feature is available on ESP target like ESP32-S3
-    };
-
-    led_indicator_strips_config_t strips_config = {
-        .led_strip_cfg = strip_config,
+    const led_indicator_strips_config_t led_indicator_gpio_config = {
+        .led_strip_cfg =
+            {
+                .strip_gpio_num = 8, // The GPIO that connected to the LED strip's data line
+                .max_leds = 1,       // The number of LEDs in the strip,
+                .color_component_format =
+                    {.format = {.g_pos = 0, .r_pos = 1, .b_pos = 2, .w_pos = 3, .num_components = 3}},
+                .led_model = LED_MODEL_WS2812, // LED strip model
+                .flags.invert_out = false,     // whether to invert the output signal
+            },
         .led_strip_driver = LED_STRIP_RMT,
-        .led_strip_rmt_cfg = rmt_config,
+        .led_strip_rmt_cfg =
+            {
+                .clk_src = RMT_CLK_SRC_DEFAULT, // different clock source can lead to different power consumption
+                .resolution_hz = 0,             // RMT counter clock frequency
+                .flags.with_dma = false,        // DMA feature is available on ESP target like ESP32-S3
+            },
     };
 
     const led_indicator_config_t config = {
-        .mode = LED_STRIPS_MODE,
-        .led_indicator_strips_config = &strips_config,
         .blink_lists = led_mode,
         .blink_list_num = BLINK_MAX,
     };
 
-    led_handle = led_indicator_create(&config);
-    assert(led_handle != NULL);
+    ESP_ERROR_CHECK(led_indicator_new_strips_device(&config, &led_indicator_gpio_config, &led_handle));
 }
